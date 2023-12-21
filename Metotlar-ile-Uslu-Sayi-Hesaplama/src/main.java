18e8fce23aef470375c4c0b5f5ff46b8053f30b2
osmankusoglu <kusogluo@gmail.com>
2023-12-22 00:07:33 +0300

    Metotlar ile Üslü Sayı Hesaplma(Recursive)

--- Metotlar-ile-Uslu-Sayi-Hesaplama/src/main.java
+++ Metotlar-ile-Uslu-Sayi-Hesaplama/src/main.java
@@ -11,14 +11,12 @@ public class main {
 
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Taban değeri giriniz : ");
+        System.out.print("Taban değeri giriniz :  ");
         int base = scan.nextInt();
-        System.out.print("Üs değerini giriniz : ");
-
+        System.out.print("Üs değerini giriniz :  ");
         int exponent = scan.nextInt();
 
         int result = power(base,exponent);
-
-        System.out.print("Sonuç : " + result);
+        System.out.print("Sonuç :  " + result);
     }
 }
